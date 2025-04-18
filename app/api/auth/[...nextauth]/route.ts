4750d09b5a925c56d722f8b364e4baa07edbef36
Josias <ndjosiasaurel@gmail.com>
2025-04-18 16:27:25 +0100

    add email provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import SlackProvider from "next-auth/providers/slack";
+import EmailProvider from "next-auth/providers/email";
+import { prisma } from "../../../util/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+const opts = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    SlackProvider({
+      clientId: process.env.SLACK_CLIENT_ID ?? "",
+      clientSecret: process.env.SLACK_CLIENT_SECRET ?? ""
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER as string,
+      from: process.env.EMAIL_FROM as string,
+      maxAge: 60 * 10 // make email links valid for 10 minutes
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }: any) {
+      // user.name = { id: user.id, display: user.name }
+      return true;
+    }
+  },
+  // secret: process.env.NEXTAUTH_SECRET,
+  debug: true
+}
+
+const handler = NextAuth(opts)
+
+export { handler as GET, handler as POST }

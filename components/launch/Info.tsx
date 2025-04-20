f3bc4997497087685ef80a32a3223814c8b762c8
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-04-19 21:48:53 -0400

    idk man stuff

--- components/launch/Info.tsx
+++ components/launch/Info.tsx
@@ -18,7 +18,12 @@ export default function Info() {
         <div className="w-screen h-screen flex flex-col items-start justify-center p-8 relative">
           <div className="bg-sky-200/30 p-6 rounded-md w-full max-w-4xl md:h-[70vh] backdrop-blur-md">
             <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">What&apos;s Shipwrecked?</h1>
-            <p className="text-base">This is more lore and information packed into the same package.</p>
+            <p className="text-base text-xl py-2">
+              On August 8-11, you and 150 other students will gather on Cathleen Stone Island in the Massachusetts Bay for a once in a lifetime, 4-day story-based hackathon! As soon as you get there, you'll all start working together to survive
+            </p>
+            <p className="text-base text-xl py-2">
+              Once we're on the island, everyone will work in smaller groups and complete quests. These will be centered around interacting with the world around you: helping the villagers develop software or hardware projects that help them sell their produce, helping the pirates plan their routes more effectively, or building projects to help your fellow shipwreck-mates organize your efforts more effectively (not literally, of course... there's no pirates or villagers in the Massachusetts bay. This is similar to Dungeons & Dragons!)
+            </p>
             <button className="mt-3 bg-sky-400 rounded p-4">How wonderful!</button>
           </div>
 

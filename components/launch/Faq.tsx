7d7c282d0af773670ca62d7ad41deb41b1b53ab5
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-04-19 23:26:43 -0400

    stuff idek

--- components/launch/Faq.tsx
+++ components/launch/Faq.tsx
@@ -17,18 +17,9 @@ export default function Faq() {
       }}>
         <div className="w-screen h-screen flex flex-col items-end justify-center p-8 relative">
           <div className="bg-sky-200/30 p-6 rounded-md w-full max-w-4xl md:h-[70vh] backdrop-blur-md">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">The Bay</h1>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">FAQ</h1>
 
-            <p className="text-base text-xl py-2">
-                Through The Bay, you'll earn an invitaiton to Shipwrecked. In The Bay you will spend 60 hours making projects with the goal of making them go viral.
-            </p>
-            <p className="text-base text-xl py-2">
-                What does going viral mean, you might wonder? Going viral means making a really polished project you are extremly proud of, which you then promote to get other people to check it out! You can find the criteria for virality <a href="https://shipwrecked.hackclub.com/info/go-viral">here</a>. Once you reach 60 hours & one of your projects has gone viral, you'll receive an invitation to Shipwrecked!.
-            </p>
-            <p className="text-base text-xl py-2">
-                Every week, you can meet up with your friends either in person or over <a href="https://pier.hackclub.com">The Pier</a> - our video game-like digital meeting space to work! You are welcome — and encouraged — to team up with a friend to make a project! Just note that if you are working in a group, you all must have log 60 hours respectively on <a href="https://hackatime.hackclub.com">Hackatime</a> towards your project.
-            </p>
-            <button className="mt-3 bg-sky-400 rounded p-4">I have more questions!</button>
+            <button className="mt-3 bg-sky-400 rounded p-4">idk text here</button>
           </div>
 
           <button className="md:hidden absolute top-5 bottom-auto left- text-6xl" onClick={() => {

7d7c1b90ea709817a7d4cfbe2cc9e500cb31eaee
Marios <xrteach@gmail.com>
2025-04-21 04:22:21 +0300

    feat: Bay Login UI

---
+++ app/bay/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginOptions from "./options";
+
+export default async function Page() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[url(/bay.png)]">
+        <img src="/logo.png" className="w-102 mb-4"></img>
+        <LoginOptions></LoginOptions>
+      </div>
+    </>
+  );
+}

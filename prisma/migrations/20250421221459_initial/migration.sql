3e3b45ebdf0e9e3b555af750e39b0dfeca77244f
Alexander Park <me@parkalex.dev>
2025-04-21 17:15:16 -0500

    Redo prisma database migrations for prod

--- prisma/migrations/20250421221459_initial/migration.sql
+++ prisma/migrations/20250421221459_initial/migration.sql
@@ -1,6 +1,22 @@
 -- CreateTable
--- Fix NextAuth Prisma Database Schema
+CREATE TABLE "Registration" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "githubUsername" TEXT NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "addressL1" TEXT NOT NULL,
+    "addressL2" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
 
+    CONSTRAINT "Registration_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT,
@@ -63,6 +79,9 @@ CREATE TABLE "Authenticator" (
 
     CONSTRAINT "Authenticator_pkey" PRIMARY KEY ("userId","credentialID")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Registration_email_key" ON "Registration"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");

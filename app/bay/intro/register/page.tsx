7c271c2a175b479582cffc077de503fa57d83bc6
Marios <xrteach@gmail.com>
2025-04-21 04:23:08 +0300

    feat: Bay Initial Registration UI

---
+++ app/bay/intro/register/page.tsx
@@ -0,0 +1,25 @@
+import ProgressBar from "@/components/common/ProgressBar";
+import { getServerSession } from "next-auth";
+import { opts } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await getServerSession(opts);
+
+  if (!session || !session?.user || !session.user.email) redirect("/bay/login");
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[url(/hut.png)]">
+        <img src="/logo-border.png" className="w-102 mb-4"></img>
+        <div className="w-102">
+          <ProgressBar
+            value={75}
+            variant="warning"
+            className="border rounded border-gray-900/60"
+          ></ProgressBar>
+        </div>
+      </div>
+    </>
+  );
+}

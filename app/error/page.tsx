489b89520d71db16af9c7dd38dd8263972acc600
Graham Darcey <graham@hackclub.com>
2025-04-22 14:58:09 -0400

    fixing yarn build

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
 export default function ErrorPage() {
-  throw new Error('This is an intentional 500 error');
+  if (typeof window !== 'undefined') {
+    throw new Error('This is an intentional 500 error');
+  }
+  return null;
 } 

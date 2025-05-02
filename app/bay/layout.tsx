20b7683942e7c7152f2e312f2c7c949074ac95d8
Josias <ndjosiasaurel@gmail.com>
2025-05-02 14:14:42 +0100

    wrap the header in the a component that provides use session

--- app/bay/layout.tsx
+++ app/bay/layout.tsx
@@ -1,10 +1,27 @@
 "use client"
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/common/Header";
+import { useSession } from "next-auth/react";
 
 export default function BayLayout({ children }: any) {
     return <>
         <SessionProvider>
-            {children}
+            <SessionWrapper>
+                {children}
+            </SessionWrapper>
         </SessionProvider>
     </>
+}
+
+function SessionWrapper({ children }: any) {
+    const { data: session, status } = useSession();
+    return (
+        <>
+            <Header 
+                session={session}
+                status={status}
+            />
+        {children}
+        </>
+    )
 }

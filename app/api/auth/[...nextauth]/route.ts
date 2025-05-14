4d3f1dd4e1d85195c4aca3ead7d10c9edee07e0a
Josias <ndjosiasaurel@gmail.com>
2025-05-14 17:43:50 +0100

    use loops to send authentication email

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,9 +4,9 @@ import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import { NextAuthOptions } from "next-auth";
-import { createTransport } from "nodemailer";
 import { randomBytes } from "crypto";
 import metrics from "@/metrics";
+import { sendEmailWithLoops } from "@/lib/loops";
 
 const adapter = {
   ...PrismaAdapter(prisma),
@@ -74,130 +74,8 @@ export const opts: NextAuthOptions = {
       sendVerificationRequest: async ({ identifier: email, url, token, provider }) => {
         // Customize the verification email
         const { host } = new URL(url);
-        const transport = await createTransport(provider.server);
         try {
-          await transport.sendMail({
-            to: email,
-            from: provider.from,
-            subject: `Sign in to Shipwrecked`,
-            text: `Click here to sign in to Shipwrecked: ${url}`,
-            html: `
-            <html>
-        <head>
-        <style>
-          .wrapper {
-            padding: 1rem;
-            margin: 0 auto;
-            max-width: 600px;
-            font-family: 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Fira Sans', 'Oxygen', 'Ubuntu', 'Helvetica Neue', sans-serif;
-          }
-
-          .container {
-            padding: 0;
-            margin: 0;
-            width: 100%;
-            max-width: 100%;
-          }
-
-          a {
-            color: #47D1F6;
-          }
-
-          .section {
-            padding: 0.5rem 1rem;
-          }
-
-          .footer {
-            font-size: 0.8rem;
-            line-height: 1.2rem;
-            color: #606a79;
-
-            background-position: center;
-            background-size: cover;
-            background-repeat: repeat-x;
-          }
-
-          .footer p {
-            margin-block-start: 0.5rem;
-            margin-block-end: 0.5rem;
-          }
-
-          .footer a {
-            color: #646464;
-          }
-        </style>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        </head>
-        <body>
-        <div class="wrapper">
-          <div class="container">
-            <table>
-              <thead>
-              <tr>
-                <th>
-                  <div class="section" style="text-align: left;">
-                    <a
-                      href="https://hackclub.com"
-                      target="_blank"
-                    >
-                      <img
-                        src="https://shipwrecked.hackclub.com/logo-outline.svg"
-                        alt="Hack Club Logo"
-                        style="width: 6rem"
-                      />
-                    </a>
-                  </div>
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>
-                  <div
-                    class="section"
-                  >
-                  <p>Hi 👋,</p>
-                  <p>You requested a login for <a href="https://shipwrecked.hackclub.com" target="_blank">Shipwrecked</a>. It's here:</p>
-                  <a href="${url}">
-                    <pre style="text-align:center;background-color:#ebebeb;padding:8px;font-size:1.5em;border-radius:4px">${url}</pre>
-                  </a>
-                  <p>- Shipwrecked Team</p>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div
-                    class="footer section"
-                    style="background-image: url('https://hackclub.com/pattern.svg');"
-                  >
-                    <p>
-                      Hack Club |
-                      <a href="mailto:team@hackclub.com">shipwrecked@hackclub.com</a>
-                      |
-                      <a href="tel:+1855625HACK">1-855-625-HACK</a>
-                    </p>
-                    <p>
-                      Hack Club is an
-                      <a href="https://hackclub.com/opensource" target="_blank">open source</a>
-                      and
-                      <a href="https://hcb.hackclub.com/hq" target="_blank"
-                        >financially transparent</a
-                      >
-                      501(c)(3) nonprofit. Our EIN is 81-2908499. By the students, for the
-                      students.
-                    </p>
-                  </div>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-
-        </body>
-        </html>
-          `,
-          });
+          await sendEmailWithLoops(host, email, url);
           metrics.increment("success.send_auth_email", 1);
         } catch (err) {
           metrics.increment("errors.send_auth_email", 1);

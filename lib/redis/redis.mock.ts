c027134a99d96286c04453620ec02ba9e39e5ce2
Josias <ndjosiasaurel@gmail.com>
2025-05-15 18:51:08 +0100

    mock redis

---
+++ lib/redis/redis.mock.ts
@@ -0,0 +1,30 @@
+
+const dummyRsvp = {
+    id: "sample",
+    firstName: "dummy",
+    lastName: "dummy",
+    email: "dummy@hackclub.com",
+    referralType: "direct",
+    referralCode: "direct",
+    createdAt: new Date().toString()
+}
+
+const redis = {
+    set: async (...args: any) => {},
+    get: async (key: string, ...args: any) => {
+        switch(key) {
+            case "rsvp_data": {
+                return dummyRsvp
+            }
+            case "rsvp_count": {
+                return 0;
+            } 
+            default:
+                return {}
+        }
+    },
+    incr: async (...args: any) => 0,
+    expire: async (...args: any) => {}
+}
+
+export default redis;

f6617e05e2796ba0a5dfe40ec43e9af0a7c5eeb1
Josias <ndjosiasaurel@gmail.com>
2025-05-15 16:29:25 +0100

    remove airtable-resolve

--- lib/airtable-resolve.ts
+++
@@ -1,7 +0,0 @@
-
-// export * from process.env.NODE_ENV === "development" ? "@/__mocks__/airtable" : "@/lib/airtable";
-if (process.env.NODE_ENV === "development") {
-  module.exports = require("@/__mocks__/airtable");
-} else {
-  module.exports = require("@/lib/airtable");
-}
